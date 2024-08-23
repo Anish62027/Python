93c13fe94b8cad0fa760f1f887457e8a39cf2d69
Anish Kumar <148037890+Anish62027@users.noreply.github.com>
2024-08-23 10:01:50 -0700

    Update top_search_results.py

--- Python Project/top5 search results From Google/top_search_results.py
+++ Python Project/top5 search results From Google/top_search_results.py
@@ -1,10 +1,7 @@
 from googlesearch import search
 
 query = "Data Science Slybas"
-
-
 search_results = search(query, num_results=5, lang="en")
-
 
 for i, result in enumerate(search_results, start=1):
     print(f"Result {i}:")
