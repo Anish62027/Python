22ac5f55aa86c4829f2a67ee134a7ac34fd526f7
Anish Kumar <148037890+Anish62027@users.noreply.github.com>
2024-08-23 10:01:15 -0700

    Update Find_Geo_And_Loc.py

--- Python Project/Current Location Find/Find_Geo_And_Loc.py
+++ Python Project/Current Location Find/Find_Geo_And_Loc.py
@@ -1,14 +1,9 @@
 import geocoder
-
-# Get the location based on the IP address
 g = geocoder.ip('me')
 
-# Get latitude and longitude
 latlng = g.latlng
 
-# Get the detailed location information
 location = g.address
 
-# Display the results
 print(f"Latitude and Longitude: {latlng}")
 print(f"Location: {location}")
