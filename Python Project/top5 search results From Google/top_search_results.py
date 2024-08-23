b1310f9e3c88d1c7cda858f5946c3cff6e3fc670
Anish Kumar <148037890+Anish62027@users.noreply.github.com>
2024-08-23 09:59:46 -0700

    Add files via upload

---
+++ Python Project/top5 search results From Google/top_search_results.py
@@ -0,0 +1,12 @@
+from googlesearch import search
+
+query = "Data Science Slybas"
+
+
+search_results = search(query, num_results=5, lang="en")
+
+
+for i, result in enumerate(search_results, start=1):
+    print(f"Result {i}:")
+    print(result)
+    print()
